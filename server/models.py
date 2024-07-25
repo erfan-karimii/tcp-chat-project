0428caea3c5499e27faac05788b3868deae17303
erfan-karimi <h410mi3@gmail.com>
2024-07-25 14:19:03 +0330

    choose password method and more

--- server/models.py
+++ server/models.py
@@ -1,12 +1,12 @@
-from server.custom_validations import ValidationError
-from server.scripts import make_password, generate_random_password
+from custom_validations import ValidationError
+from scripts import make_password, generate_random_password
 
 
 class ChatUser:
-    def __init__(self, client, nickname):
-        self.client = client
+    def __init__(self, conn, nickname, password=None):
+        self.conn = conn
         self.nickname = nickname
-        self.password = None
+        self.password = password
         self.role = None
 
     def change_password(self, old_password, new_passwod, new_password_2):
@@ -21,5 +21,16 @@ class ChatUser:
         _, random_password = generate_random_password()
         self.password = random_password
 
-    def set_password(self, password):
-        self.password = make_password(password)
+    def set_password(self):
+        self.password = make_password(self.password.encode("utf-8"))
+
+    def choose_password(self, password_choices, **kwargs):
+        if password_choices not in ["0", "1", "2"]:
+            raise ValueError("wrong password_choices value")
+
+        method_map = {
+            "0": self.set_password,
+            "1": self.set_random_password,
+            "2": self.change_password,
+        }
+        method_map[password_choices](**kwargs)
